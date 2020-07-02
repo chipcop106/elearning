d35fd2429eecbf218023ecd3d5fab39c228dec15
Hieu Nguyen <hieu2091@gmail.com>
2020-07-02 16:08:39 +0700

    add import

--- src/js/components/TeacherDetail/StudentCommentItem.js
+++ src/js/components/TeacherDetail/StudentCommentItem.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const StudentCommentItem = ({ name, avatar, content, date, time }) => {
   return (
     <div className="tc-comment">
