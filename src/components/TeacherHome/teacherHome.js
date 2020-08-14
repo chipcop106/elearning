3b454a2f76f465c4ccca449d52dbbcc7a654a7e5
DatCodero <chipcop106@gmail.com>
2020-08-14 11:39:26 +0700

    Change layout

--- src/components/TeacherHome/teacherHome.js
+++ src/components/TeacherHome/teacherHome.js
@@ -8,7 +8,7 @@ import Flatpickr from 'react-flatpickr';
 import TeacherSidebar from './TeacherSidebar';
 import UpComingList from './UpComingList';
 import { ToastContainer } from 'react-toastify';
-import {appSettings} from '~src/config';
+import { appSettings } from '~src/config';
 import Select from 'react-select';
 const DateTimeFormat = new Intl.DateTimeFormat('vi-VN', {
     dateStyle: 'short',
@@ -18,12 +18,16 @@ const DateTimeFormat = new Intl.DateTimeFormat('vi-VN', {
 
 const itemShowOptions = [
     {
-        value:5,
-        label:'Last 5 lesson'
+        value: 999999,
+        label: 'All lessons'
     },
     {
-        value:10,
-        label:'Last 10 lesson'
+        value: 5,
+        label: 'Last 5 lessons'
+    },
+    {
+        value: 10,
+        label: 'Last 10 lessons'
     }
 ]
 
@@ -33,10 +37,10 @@ const SituationBlock = ({ title, value, unit, imageUrl, link, linkTitle, isLoadi
             <div className="card-body d-flex align-items-center justify-content-between bd-0-f pd-20">
                 <div className="flex-shrink-0 d-flex flex-column justify-content-between">
                     {imageUrl ? <img src={imageUrl} className="wd-75 ht-50 object-fit mg-b-10" /> : <Skeleton circle={true} />}
-                    <p className="tx-18 text-center mg-b-0 tx-medium">{!isLoading ? (title) : (<Skeleton />)}</p>
-                    <a href={link ? link : ''} >{!isLoading ? (linkTitle) : (<Skeleton />)}</a>
+                    <p className="tx-14 text-center mg-b-0 tx-medium">{!isLoading ? (title) : (<Skeleton />)}</p>
+                    <a className="tx-12" href={link ? link : ''} >{!isLoading ? (linkTitle) : (<Skeleton />)}</a>
                 </div>
-                <div className="tx-center circle-value">
+                <div className="tx-center circle-value flex-shrink-0">
                     <p className="mg-b-0 tx-20 tx-bold tx-gray-600">{!isLoading ? (value) : (<Skeleton />)}</p>
                     <p className="text-center mg-b-0 tx-gray-400">{!isLoading ? (unit) : (<Skeleton />)}</p>
                 </div>
@@ -97,84 +101,90 @@ const TeacherHome = () => {
                     <div className="flex-grow-1 pd-xl-x-15 wd-100p">
                         <div className="gv-situation mg-b-15">
                             {/* <div className="mg-b-15 d-lg-flex align-items-center justify-content-between">
-                                <div className="form-row from-to-group" id="filter-time">
-                                    <div className="wd-sm-200 col">
+                                <h5 className="mg-b-0">Teaching Situation</h5>
+                                <div className="d-flex flex-wrap-0 form-row pd-x-5 flex-grow-1 justify-content-end" id="filter-time">
+                                    <div className="wd-sm-200 pd-x-5 wd-100p mg-b-10 mg-sm-b-0">
                                         <Flatpickr
-                                            placeholder="From date"
                                             options={{
                                                 dateFormat: "d/m/Y",
+                                                mode: 'single',
+                                                maxDate: new Date()
                                             }}
                                             className="form-control"
                                             onChange={(date) => setFromDate(date)}
+                                            placeholder="From date"
                                         />
                                     </div>
-                                    <div className="wd-sm-200 col">
+                                    <div className="wd-sm-200 pd-x-5 wd-100p">
                                         <Flatpickr
-                                            placeholder="To date"
                                             options={{
                                                 dateFormat: "d/m/Y",
+                                                maxDate: new Date(),
+                                                mode: 'single',
                                                 onOpen: function (selectedDates, dateStr, instance) {
-                                                    console.log(instance);
-                                                    if (fromDate === '') return;
+                                                    if (fromDate.length === 0) {
+                                                        instance.set("minDate", null);
+                                                        return;
+                                                    }
                                                     instance.set("minDate", new Date(fromDate));
-
                                                 }
                                             }}
                                             className="form-control"
                                             onChange={(date) => setToDate(date)}
+                                            placeholder="To date"
+
                                         />
                                     </div>
                                     <div className="flex-grow-0 tx-right flex-shrink-0 pd-x-5">
-                                        <button type="button" className="btn btn-info " onClick={_onFilterDate}><i className="fa fa-search" /></button>
+                                        <button type="button" className="btn btn-primary " onClick={_onFilterDate}><i className="fa fa-search" /></button>
                                     </div>
                                 </div>
                             </div> */}
                             <div className="row">
                                 <div className="col-12 col-md-4 mg-b-15">
-                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.OpenSlotURL} linkTitle="Manage slot" title="Open" value={dashboardInfo?.OpenSlot} unit="slots" imageUrl={'../assets/img/slot-open.png'} />
+                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.OpenSlotURL} linkTitle="Manage slot" title="Slots Opened" value={dashboardInfo?.OpenSlot} unit="slots" imageUrl={'../assets/img/slot-open.png'} />
                                 </div>
                                 <div className="col-12 col-md-4  mg-b-15">
-                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.BookedSlotURl} linkTitle="Manage slot" title="Booked" value={dashboardInfo?.BookedSlot} unit="slots" imageUrl={'../assets/img/slot-booked.png'} />
+                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.BookedSlotURl} linkTitle="Manage slot" title="Slots Booked" value={dashboardInfo?.BookedSlot} unit="slots" imageUrl={'../assets/img/slot-booked.png'} />
                                 </div>
                                 <div className="col-12 col-md-4  mg-b-15">
-                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.FeedbackURL} linkTitle="View feedback" title="Missing" value={dashboardInfo?.Feedback} unit="Feedback" imageUrl={'../assets/img/missing-feedback.png'} />
+                                    <SituationBlock isLoading={isLoading} link={dashboardInfo?.FeedbackURL} linkTitle="View feedback" title="Missing Feedbacks" value={dashboardInfo?.Feedback} unit="Feedback" imageUrl={'../assets/img/missing-feedback.png'} />
                                 </div>
                             </div>
-
                         </div>
                         <div className="row">
                             <div className="col-lg-12">
-                            <div className="card">
-                                <div className="card-header align-items-center d-flex justify-content-between">
-                                    <div className="d-flex align-items-center">
-                                        <div className="">
-                                            <h5 className="mg-b-5">Upcoming lesson</h5>
-                                            <p className="tx-gray-300 mg-b-0">Next upcoming lesson with student</p>
+                                <div className="card">
+
+                                    <div className="card-body">
+                                        <div className="align-items-center d-flex justify-content-between pd-b-15">
+                                            <div className="d-flex align-items-center">
+                                                <div className="">
+                                                    <h5 className="mg-b-0">Upcoming lesson</h5>
+                                                </div>
+                                            </div>
+                                            <div className="wd-150">
+                                                <Select
+                                                    options={itemShowOptions}
+                                                    styles={appSettings.selectStyle}
+                                                    onChange={setSelectShow}
+                                                    defaultValue={selectShow}
+                                                />
+                                            </div>
+
+                                        </div>
+                                        <div className="gv-notice">
+                                            <UpComingList itemShow={selectShow} />
                                         </div>
                                     </div>
-                                    <div className="wd-150">
-                                        <Select 
-                                            options={itemShowOptions}
-                                            styles={appSettings.selectStyle}
-                                            onChange={setSelectShow}
-                                            defaultValue={selectShow}
-                                        />
-                                    </div>
-                                   
                                 </div>
-                                <div className="card-body">
-                                    <div className="gv-notice">
-                                        <UpComingList itemShow={selectShow}/>
-                                    </div>
-                                </div>
+
+
                             </div>
-                               
-                               
-                            </div>
-                           
+
                         </div>
-                     
-                       
+
+
                     </div>
                 </div>
             </div>
